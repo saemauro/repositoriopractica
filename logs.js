88138c0b9fc26da8728cd0f2e614ac51f7871976
saemauro <maurosaegh@gmail.com>
2023-01-16 21:12:15 -0300

    Nuevos archivos

---
+++ logs.js
@@ -0,0 +1,4 @@
+console.log("Hola1");
+console.log("Hola2");
+console.log("Hola3");
+console.log("Hola4");
---
+++ logs2.js
@@ -0,0 +1,7 @@
+console.log("Hola1");
+console.log("Hola2");
+console.log("Hola3");
+console.log("Hola4");
+console.log("Hola5");
+console.log("Hola6");
+console.log("Hola7");
